c998c854dd3177df7e8b7421734eaa77e1f8e2ac
Tobias Schneider <toby@hpxl.io>
2014-05-11 16:21:07 +0200

    ST3 Project, code commented, Screen class tests

--- retro.js
+++ retro.js
@@ -3,8 +3,6 @@ var blessed = require('blessed'),
 	widgets = retro.Widgets,
 	amazon = retro.API;
 
-// screen.append(blessed.line());
-
 
 var array = ['{Q}uit', '{N}ext', '{ESC} Cancel', '{RET} Quantity'];
 var title = widgets.title({
@@ -25,9 +23,62 @@ var content = blessed.box({
         bg: 'black'
     }
 });
-var screen = new widgets.screen();
-screen.title = title;
 
+var test = blessed.box({
+    top: 1,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    content: 'TEST CONTENT'
+})
+
+// TODO: lists still throw an error, even if a blessed.line() is appended under screen.js
+// var list = blessed.list({
+//     top: 1,
+//     left: 0,
+//     align: 'center',
+//     fg: 'blue',
+//     bg: 'default',
+//     keys: true,
+//     border: {
+//         type: 'ascii',
+//         fg: 'default',
+//         bg: 'default'
+//     },
+//     width: '50%',
+//     height: '50%',
+//     top: 'center',
+//     left: 'center',
+//     items: ['hallo'],
+//     selectedBg: 'green',
+//     scrollbar: {
+//         ch: ' ',
+//         track: {
+//             bg: 'yellow'
+//         },
+//         style: {
+//             inverse: true
+//         }
+//     }
+// });
+
+var input = blessed.textarea({
+  parent: screen,
+  // Possibly support:
+  // align: 'center',
+  bg: 'blue',
+  fg: 'red',
+  height: '50%',
+  width: '50%',
+  top: 'center',
+  left: 'center',
+  content:'enter text',
+});
+
+var screen = new widgets.screen();
+
+screen.title = title;
+screen.content = test;
 screen.commandbar = commandbar;
 
 widgets.screen.switchScreen(screen);
